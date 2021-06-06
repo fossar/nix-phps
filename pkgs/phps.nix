a7510fa0c9d25189131a703952d45fb8c54c4ba9
Jan Tojnar <jtojnar@gmail.com>
2021-06-06 07:42:24 +0200

    Move package overrides into a separate file
    1. Move it out.
    2. Pass it just `prev` from the overlay as pkgs.
    3. Since there is no collision with the overlay attributes, rename the arguments to final & prev.
    4. Pass the overrides directly to PHPs from Nixpkgs instead of taking it from _args.
    
    This will make it more convenient for us to change _mkArgs into a function.

--- pkgs/phps.nix
+++ pkgs/phps.nix
@@ -5,189 +5,12 @@ nixpkgs:
 final: prev:
 
 let
-  patchName = patch: patch.name or (builtins.baseNameOf patch);
+  packageOverrides = import ./package-overrides.nix prev;
 
   _args = {
     inherit (prev) callPackage lib stdenv nixosTests;
 
-    packageOverrides = self: super: {
-      tools = super.tools // {
-        php-cs-fixer-2 = self.callPackage ./php-cs-fixer/2.x.nix { };
-      };
-
-      extensions = super.extensions // {
-        apcu =
-          if prev.lib.versionOlder super.php.version "7.0" then
-            super.extensions.apcu.overrideAttrs (attrs: {
-              name = "apcu-4.0.11";
-              version = "4.0.11";
-              src = builtins.fetchurl {
-                url = "http://pecl.php.net/get/apcu-4.0.11.tgz";
-                sha256 = "002d1gklkf0z170wkbhmm2z1p9p5ghhq3q1r9k54fq1sq4p30ks5";
-              };
-            })
-          else
-            super.extensions.apcu;
-
-        dom = super.extensions.dom.overrideAttrs (attrs: {
-          patches =
-            let
-              upstreamPatches =
-                attrs.patches or [];
-
-              ourPatches = prev.lib.optionals (prev.lib.versionOlder super.php.version "7.2") [
-                # Fix tests with libxml2 2.9.10.
-                (prev.fetchpatch {
-                  url = "https://github.com/php/php-src/commit/e29922f054639a934f3077190729007896ae244c.patch";
-                  sha256 = "zC2QE6snAhhA7ItXgrc80WlDVczTlZEzgZsD7AS+gtw=";
-                })
-              ];
-            in
-            ourPatches ++ upstreamPatches;
-
-          preCheck =
-            prev.lib.pipe
-              attrs.preCheck
-              [
-                (preCheck:
-                  if prev.lib.versionOlder super.php.version "7.3" then
-                    # Test not available on older versions.
-                    # Introduced in https://github.com/NixOS/nixpkgs/pull/124193
-                    builtins.replaceStrings [ "rm tests/bug80268.phpt" ] [ "" ] preCheck
-                  else
-                    preCheck
-                )
-              ];
-        });
-
-        intl = super.extensions.intl.overrideAttrs (attrs: {
-          doCheck = if prev.lib.versionOlder super.php.version "7.2" then false else attrs.doCheck or true;
-          patches =
-            let
-              upstreamPatches =
-                attrs.patches or [];
-
-              ourPatches =
-                prev.lib.optionals (prev.lib.versionOlder super.php.version "7.1") [
-                  # Fix build with newer ICU.
-                  (prev.fetchpatch {
-                    url = "https://github.com/php/php-src/commit/8d35a423838eb462cd39ee535c5d003073cc5f22.patch";
-                    sha256 = if prev.lib.versionOlder super.php.version "7.0" then "8v0k6zaE5w4yCopCVa470TMozAXyK4fQelr+KuVnAv4=" else "NO3EY5z1LFWKor9c/9rJo1rpigG5x8W3Uj5+xAOwm+g=";
-                    postFetch = ''
-                      patch "$out" < ${if prev.lib.versionOlder super.php.version "7.0" then ./intl-icu-patch-5.6-compat.patch else ./intl-icu-patch-7.0-compat.patch}
-                    '';
-                  })
-                ];
-            in
-            ourPatches ++ upstreamPatches;
-        });
-
-        iconv = super.extensions.iconv.overrideAttrs (attrs: {
-          patches =
-            let
-              upstreamPatches =
-                attrs.patches or [];
-
-              ourPatches =
-                prev.lib.optionals (prev.lib.versionOlder super.php.version "8.0") [
-                  # Header path defaults to FHS location, preventing the configure script from detecting errno support.
-                  # TODO: re-add when PHP 7 is removed from Nixpkgs.
-                  # ./iconv-header-path.patch
-                ];
-            in
-            ourPatches ++ upstreamPatches;
-        });
-
-        memcached =
-          if prev.lib.versionOlder super.php.version "7.0" then
-            super.extensions.memcached.overrideAttrs (attrs: {
-              name = "memcached-2.2.0";
-              version = "2.2.0";
-              src = builtins.fetchurl {
-                url = "http://pecl.php.net/get/memcached-2.2.0.tgz";
-                sha256 = "0n4z2mp4rvrbmxq079zdsrhjxjkmhz6mzi7mlcipz02cdl7n1f8p";
-              };
-            })
-          else
-            super.extensions.memcached;
-
-        mysqlnd =
-          if prev.lib.versionOlder super.php.version "7.1" then
-            super.extensions.mysqlnd.overrideAttrs (attrs: {
-              # Fix mysqlnd not being able to find headers.
-              postPatch = attrs.postPatch or "" + "\n" + ''
-                ln -s $PWD/../../ext/ $PWD
-              '';
-            })
-          else
-            super.extensions.mysqlnd;
-
-        oci8 =
-          if prev.lib.versionOlder super.php.version "7.0" then
-            super.extensions.oci8.override ({
-              version = "2.0.12";
-              sha256 = "1khqa7fs8dbyjclx05a5ls1f8paw1ij21qwlx3v7p8i3iqhnymkj";
-            })
-          else
-            super.extensions.oci8;
-
-        opcache = super.extensions.opcache.overrideAttrs (attrs: {
-          patches =
-            builtins.filter
-              (patch:
-                # The patch do not apply to PHP 5’s opcache.
-                patchName patch == "zend_file_cache_config.patch" -> prev.lib.versionAtLeast super.php.version "7.0"
-              )
-              (attrs.patches or []);
-        });
-
-        openssl =
-          if prev.lib.versionOlder super.php.version "7.1" then
-            super.extensions.openssl.overrideAttrs (attrs: {
-              # PHP ≤ 7.0 requires openssl 1.0.
-              buildInputs =
-                let
-                  openssl_1_0_2 = prev.openssl_1_0_2.overrideAttrs (attrs: {
-                    meta = attrs.meta // {
-                      # It is insecure but that should not matter in an isolated test environment.
-                      knownVulnerabilities = [];
-                    };
-                  });
-                in
-                  map (p: if p == prev.openssl then openssl_1_0_2 else p) attrs.buildInputs or [];
-              })
-          else
-            super.extensions.openssl;
-
-        readline = super.extensions.readline.overrideAttrs (attrs: {
-          patches =
-            let
-              upstreamPatches =
-                attrs.patches or [];
-
-              ourPatches =
-                prev.lib.optionals (prev.lib.versionOlder super.php.version "7.2") [
-                  # Fix readline build
-                  (prev.fetchpatch {
-                    url = "https://github.com/php/php-src/commit/1ea58b6e78355437b79fb7b1f287ba6688fb1c57.patch";
-                    sha256 = "Lh2h07lKkAXpyBGqgLDNXeiOocksARTYIysLWMon694=";
-                  })
-                ];
-            in
-            ourPatches ++ upstreamPatches;
-        });
-
-        zlib = super.extensions.zlib.overrideAttrs (attrs: {
-          patches =
-            builtins.filter
-              (patch:
-                # The patch does not apply to PHP 7’s zlib.
-                patchName patch == "zlib-darwin-tests.patch" -> prev.lib.versionAtLeast super.php.version "7.1"
-              )
-              (attrs.patches or []);
-        });
-      };
-    };
+    inherit packageOverrides;
   };
 
   generic = (import "${nixpkgs}/pkgs/development/interpreters/php/generic.nix") _args;
@@ -245,14 +68,14 @@ in {
   ] ++ prev.lib.optionals (!prev.stdenv.isDarwin) [ imap ]));
 
   php73 = prev.php73.override {
-    inherit (_args) packageOverrides;
+    inherit packageOverrides;
   };
 
   php74 = prev.php74.override {
-    inherit (_args) packageOverrides;
+    inherit packageOverrides;
   };
 
   php80 = prev.php80.override {
-    inherit (_args) packageOverrides;
+    inherit packageOverrides;
   };
 }
