2d3d51e08f688fbdd308bf1835cb31151bb383de
Jan Tojnar <jtojnar@gmail.com>
2021-06-13 09:27:36 +0200

    flake: Update dependencies
    The generic.nix has been simplified.

--- pkgs/phps.nix
+++ pkgs/phps.nix
@@ -29,7 +29,7 @@ let
         });
     } // args;
 
-  generic = prev.callPackage "${nixpkgs}/pkgs/development/interpreters/php/generic.nix" { };
+  generic = "${nixpkgs}/pkgs/development/interpreters/php/generic.nix";
 
   base56 = prev.callPackage generic (_mkArgs {
     version = "5.6.40";
