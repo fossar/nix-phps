86a55392e67e01051da31c55dbff17b2b7211569
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-01-25 11:05:56 +0100

    Bump xdebug extension.

--- pkgs/package-overrides.nix
+++ pkgs/package-overrides.nix
@@ -294,7 +294,7 @@ in
           name = "xdebug-3.1.2";
           version = "3.1.2";
           src = pkgs.fetchurl {
-            url = "http://pecl.php.net/get/xdebug-3.1.1.tgz";
+            url = "http://pecl.php.net/get/xdebug-3.1.2.tgz";
             sha256 = "1wycg5kzm15dq6db3yc0xgq3baqbjg1sz784vz5rrxqd23hnngq8";
           };
         })
