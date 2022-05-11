9c82c8af3cf850e4c6aa8d5f770e6af88b1c4618
Jan Tojnar <jtojnar@gmail.com>
2022-05-11 15:11:00 +0200

    checks: Move most of the code to flake.nix
    This will basically allow us to treat `checks.nix` like something that supports having its dependencies injected by `callPackages`, allowing us to simplify the expression a bit. It will also allow other scripts to get the check names trivially.

--- checks.nix
+++ checks.nix
@@ -1,158 +1,96 @@
 {
-  system,
-  packages,
-  pkgs,
+  lib,
+  php,
+  runCommand,
 }:
 
-let
-  phpPackages = builtins.filter (name: builtins.match "php[0-9]+" name != null) (builtins.attrNames packages);
-
-  checks = {
-    php = {
-      description = "Build PHP";
-      drv = { php, ... }: php;
-    };
-
-    imagick = {
-      description = "Build Imagick extension";
-      drv = { php, ... }: php.extensions.imagick;
-    };
-
-    redis = {
-      description = "Build Redis extension";
-      drv = { php, ... }: php.extensions.redis;
-    };
-
-    redis3 = {
-      description = "Build Redis 3 extension";
-      enabled = { php, lib, ... }: lib.versionOlder php.version "8";
-      drv = { php, ... }: php.extensions.redis3;
-    };
-
-    mysql = {
-      description = "Build MySQL extension";
-      enabled = { php, lib, ... }: lib.versionOlder php.version "7";
-      drv = { php, ... }: php.extensions.mysql;
-    };
-
-    xdebug = {
-      description = "Build Xdebug extension";
-      drv = { php, ... }: php.extensions.xdebug;
-    };
-
-    tidy = {
-      description = "Build Tidy extension";
-      drv = { php, ... }: php.extensions.tidy;
-    };
-
-    composer-phar = {
-      description = "Check that composer PHAR works";
-      drv =
-        { pkgs, php, ... }:
-        pkgs.runCommand
-          "composer-phar-check"
-          {
-            buildInputs = [
-              php.packages.composer
-            ];
-          }
-          ''
-            composer --version
-            touch "$out"
-          '';
-    };
-
-    mysqli-socket-path = {
-      description = "Validate php.extensions.mysqli default unix socket path";
-      drv =
-        { pkgs, php, ... }:
-        pkgs.runCommand
-          "mysqli-socket-path-check"
-          {
-            buildInputs = [
-              (php.withExtensions ({ all, ... }: [
-                all.mysqli
-              ]))
-            ];
-          }
-          ''
-            php -r "echo ini_get('mysqli.default_socket') . PHP_EOL;" | grep /run/mysqld/mysqld.sock
-            touch "$out"
-          '';
-    };
-
-    pdo_mysql-socket-path = {
-      description = "Validate php.extensions.pdo_mysql default unix socket path";
-      drv =
-        { pkgs, php, ... }:
-        pkgs.runCommand
-          "pdo_mysql-socket-path-check"
-          {
-            buildInputs = [
-              (php.withExtensions ({ all, ... }: [
-                all.pdo_mysql
-              ]))
-            ];
-          }
-          ''
-            php -r "echo ini_get('pdo_mysql.default_socket') . PHP_EOL;" | grep /run/mysqld/mysqld.sock
-            touch "$out"
-          '';
-    };
+{
+  php = {
+    description = "Build PHP";
+    drv = php;
   };
 
-  inherit (pkgs) lib;
+  imagick = {
+    description = "Build Imagick extension";
+    drv = php.extensions.imagick;
+  };
 
-  /* AttrSet<phpName, AttrSet<checkName, checkDrv>> */
-  checksPerVersion =
-    lib.listToAttrs (
-      builtins.map
-        (phpName:
-          let
-            php = packages.${phpName};
-            phpVersion = lib.versions.majorMinor php.version;
-            args = { inherit lib php pkgs system; };
-            supportedChecks = lib.filterAttrs (_name: { enabled ? lib.const true, ... }: enabled args) checks;
-          in
-          {
-            name = phpName;
-            value =
-              lib.mapAttrs
-                (
-                  _name:
-                  {
-                    description,
-                    drv,
-                    ...
-                  }:
+  redis = {
+    description = "Build Redis extension";
+    drv = php.extensions.redis;
+  };
 
-                  let
-                    check = drv args;
-                  in
-                  check // {
-                    description = "PHP ${phpVersion} – ${description}";
-                  }
-                )
-                supportedChecks;
-          }
-        )
-        phpPackages
-    );
-in
-lib.foldAttrs
-  lib.mergeAttrs
-  {}
-  (
-    lib.mapAttrsToList
-    (
-      phpName:
+  redis3 = {
+    description = "Build Redis 3 extension";
+    enabled = lib.versionOlder php.version "8";
+    drv = php.extensions.redis3;
+  };
 
-      lib.mapAttrs'
-        (
-          name:
+  mysql = {
+    description = "Build MySQL extension";
+    enabled = lib.versionOlder php.version "7";
+    drv = php.extensions.mysql;
+  };
 
-          lib.nameValuePair "${phpName}-${name}"
-        )
-    )
-    checksPerVersion
-  )
+  xdebug = {
+    description = "Build Xdebug extension";
+    drv = php.extensions.xdebug;
+  };
+
+  tidy = {
+    description = "Build Tidy extension";
+    drv = php.extensions.tidy;
+  };
+
+  composer-phar = {
+    description = "Check that composer PHAR works";
+    drv =
+      runCommand
+        "composer-phar-check"
+        {
+          buildInputs = [
+            php.packages.composer
+          ];
+        }
+        ''
+          composer --version
+          touch "$out"
+        '';
+  };
+
+  mysqli-socket-path = {
+    description = "Validate php.extensions.mysqli default unix socket path";
+    drv =
+      runCommand
+        "mysqli-socket-path-check"
+        {
+          buildInputs = [
+            (php.withExtensions ({ all, ... }: [
+              all.mysqli
+            ]))
+          ];
+        }
+        ''
+          php -r "echo ini_get('mysqli.default_socket') . PHP_EOL;" | grep /run/mysqld/mysqld.sock
+          touch "$out"
+        '';
+  };
+
+  pdo_mysql-socket-path = {
+    description = "Validate php.extensions.pdo_mysql default unix socket path";
+    drv =
+      runCommand
+        "pdo_mysql-socket-path-check"
+        {
+          buildInputs = [
+            (php.withExtensions ({ all, ... }: [
+              all.pdo_mysql
+            ]))
+          ];
+        }
+        ''
+          php -r "echo ini_get('pdo_mysql.default_socket') . PHP_EOL;" | grep /run/mysqld/mysqld.sock
+          touch "$out"
+        '';
+  };
+}
