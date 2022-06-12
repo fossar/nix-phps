1e3a4f70c30718b807c223ce041723d86b2ddbe2
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-06-12 09:49:35 +0200

    flake.lock: Update
    Also add `php74` back since it has been removed upstream
    in https://github.com/NixOS/nixpkgs/pull/174555.

---
+++ pkgs/php/7.4.nix
@@ -0,0 +1,60 @@
+{ prev, mkPhp }:
+
+let
+  base = mkPhp {
+    version = "7.4.30";
+    sha256 = "tgG7EuU3IEabYOqBZ3bKwcBpawmIihGtI3my7ug1OG4=";
+  };
+in
+base.withExtensions (
+  { all, ... }:
+
+  with all; (
+    [
+      bcmath
+      calendar
+      curl
+      ctype
+      dom
+      exif
+      fileinfo
+      filter
+      ftp
+      gd
+      gettext
+      gmp
+      iconv
+      intl
+      json
+      ldap
+      mbstring
+      mysqli
+      mysqlnd
+      opcache
+      openssl
+      pcntl
+      pdo
+      pdo_mysql
+      pdo_odbc
+      pdo_pgsql
+      pdo_sqlite
+      pgsql
+      posix
+      readline
+      session
+      simplexml
+      sockets
+      soap
+      sodium
+      sqlite3
+      tokenizer
+      xmlreader
+      xmlwriter
+      zip
+      zlib
+    ]
+    ++ prev.lib.optionals (!prev.stdenv.isDarwin) [
+      imap
+    ]
+  )
+)
