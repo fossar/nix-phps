f560c09d8e1e33cd774b85bb8c931841d3310b2f
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-06-13 22:23:42 +0200

    Create separate files for each PHP version.

---
+++ pkgs/php/7.1.nix
@@ -0,0 +1,59 @@
+{ prev, mkPhp }:
+
+let
+  base = mkPhp {
+    version = "7.1.33";
+    sha256 = "laXl8uK3mzdrc3qC2WgskYkeYCifokGDRjoqyhWPT0s=";
+  };
+in
+base.withExtensions (
+  { all, ... }:
+
+  with all; (
+    [
+      bcmath
+      calendar
+      curl
+      ctype
+      dom
+      exif
+      fileinfo
+      filter
+      ftp
+      gd
+      gettext
+      gmp
+      iconv
+      intl
+      json
+      ldap
+      mbstring
+      mysqli
+      mysqlnd
+      opcache
+      openssl
+      pcntl
+      pdo
+      pdo_mysql
+      pdo_odbc
+      pdo_pgsql
+      pdo_sqlite
+      pgsql
+      posix
+      readline
+      session
+      simplexml
+      sockets
+      soap
+      sqlite3
+      tokenizer
+      xmlreader
+      xmlwriter
+      zip
+      zlib
+    ]
+    ++ prev.lib.optionals (!prev.stdenv.isDarwin) [
+      imap
+    ]
+  )
+)
