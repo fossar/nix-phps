fossar/nix-phps

commit 1a671f05bd028b0ea0690ec19bd5ccda68998c59

fix: fix `xdebug` extension for PHP 7.
Related to https://github.com/NixOS/nixpkgs/pull/205180

pkgs/package-overrides.nix
@@ -476,16 +476,16 @@ in
     xdebug =
       # xdebug versions were determined using https://xdebug.org/docs/compat
       if lib.versionAtLeast prev.php.version "8.2" then
+        prev.extensions.xdebug
+      else if lib.versionAtLeast prev.php.version "7.2" then
         prev.extensions.xdebug.overrideAttrs (attrs: {
-          name = "xdebug-3.2.0RC2";
-          version = "3.2.0RC2";
+          name = "xdebug-3.1.6";
+          version = "3.1.6";
           src = pkgs.fetchurl {
-            url = "http://pecl.php.net/get/xdebug-3.2.0RC2.tgz";
-            sha256 = "dQgXDP3Ifg+D0niWxaJ4ec71Vfr8KH40jv6QbxSyY+4=";
+            url = "http://pecl.php.net/get/xdebug-3.1.6.tgz";
+            sha256 = "1lnmrb5kgq8lbhjs48j3wwhqgk44pnqb1yjq4b5r6ysv9l5wlkjm";
           };
         })
-      else if lib.versionAtLeast prev.php.version "7.2" then
-        prev.extensions.xdebug
       else if lib.versionAtLeast prev.php.version "7.1" then
         prev.extensions.xdebug.overrideAttrs (attrs: {
           name = "xdebug-2.9.8";

commit 6f378774fa70fa6511f54e3e24405418989062d8

`xdebug` 3.2.0 works from php 8.0 to php 8.2
Issue introduced in https://github.com/fossar/nix-phps/commit/1a671f05bd028b0ea0690ec19bd5ccda68998c59
XDebug 3.2.0 is compatible from PHP 8.0, this PR reflects that.

pkgs/package-overrides.nix
@@ -475,7 +475,7 @@ in
 
     xdebug =
       # xdebug versions were determined using https://xdebug.org/docs/compat
-      if lib.versionAtLeast prev.php.version "8.2" then
+      if lib.versionAtLeast prev.php.version "8.0" then
         prev.extensions.xdebug
       else if lib.versionAtLeast prev.php.version "7.2" then
         prev.extensions.xdebug.overrideAttrs (attrs: {