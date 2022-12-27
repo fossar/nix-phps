293836fb0b0b9af1e066460268e7e4278ef44d55
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-12-27 09:00:44 +0100

    add default nix formatter and its corresponding Github action

--- flake.nix
+++ flake.nix
@@ -28,6 +28,8 @@
           ];
         };
       in rec {
+        formatter = pkgs.alejandra;
+
         packages = {
           inherit (pkgs) php php56 php70 php71 php72 php73 php74 php80 php81 php82;
         };
