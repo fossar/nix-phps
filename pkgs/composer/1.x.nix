70b8af7ca11e22dd74cbe6970cb6491c89c8bb31
Soner Sayakci <s.sayakci@shopware.com>
2023-01-14 11:27:52 +0100

    feat: add composer 1

---
+++ pkgs/composer/1.x.nix
@@ -0,0 +1,35 @@
+{ mkDerivation, fetchurl, makeWrapper, unzip, lib, php }:
+let
+  pname = "composer";
+  version = "1.10.26";
+in
+mkDerivation {
+  inherit pname version;
+
+  src = fetchurl {
+    url = "https://getcomposer.org/download/${version}/composer.phar";
+    sha256 = "sha256-y/4fhSdsV6vkZNk0UD2TWqITSUrChidcjfq/qR49vcQ=";
+  };
+
+  dontUnpack = true;
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    install -D $src $out/libexec/composer/composer.phar
+    makeWrapper ${php}/bin/php $out/bin/composer \
+      --add-flags "$out/libexec/composer/composer.phar" \
+      --prefix PATH : ${lib.makeBinPath [ unzip ]}
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Dependency Manager for PHP";
+    license = licenses.mit;
+    homepage = "https://getcomposer.org/";
+    changelog = "https://github.com/composer/composer/releases/tag/${version}";
+    maintainers = with maintainers; [ offline ] ++ teams.php.members;
+  };
+}
