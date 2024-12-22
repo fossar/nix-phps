323d9eb87385473078ebdeef8873530c4f7dbbd7
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-12-22 23:03:54 +0100

    phpExtensions.redis: fix build for 7.1 <= PHP <= 7.2

--- pkgs/package-overrides.nix
+++ pkgs/package-overrides.nix
@@ -696,7 +696,7 @@ in
     redis =
       if lib.versionOlder prev.php.version "7.0" then
         final.callPackage ./extensions/redis/4.nix { }
-      else if lib.versionOlder prev.php.version "7.1" then
+      else if lib.versionOlder prev.php.version "7.3" then
         final.callPackage ./extensions/redis/6.0.nix { }
       else if lib.versionOlder prev.php.version "8.0" then
         prev.extensions.redis.overrideAttrs (attrs: {
