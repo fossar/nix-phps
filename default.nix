10759cd8d47c675ddff160e45cc300e71adf4402
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-12-28 10:42:42 +0100

    chore: replace `edolstra/flake-compat` with `NixOS/flake-compat`

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@
 let
   lock = builtins.fromJSON (builtins.readFile ./flake.lock);
   flake-compat = builtins.fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    url = "https://github.com/NixOS/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
     sha256 = lock.nodes.flake-compat.locked.narHash;
   };
   flake = import flake-compat {
